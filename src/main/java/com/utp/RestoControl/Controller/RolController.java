688127fbac7be0576b95f59807e69d5096caf23c
MarcosHV0106 <marcoshv2005@gmail.com>
2026-05-27 08:10:56 -0500

    Commit donde se agregó Roles

---
+++ src/main/java/com/utp/RestoControl/Controller/RolController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.utp.RestoControl.Controller;
+
+import com.utp.RestoControl.Entity.Rol;
+import com.utp.RestoControl.Service.RolService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/roles")
+
+@RequiredArgsConstructor
+public class RolController {
+    
+    private final RolService service;
+    
+    @GetMapping
+    public List<Rol> listar(){
+        return service.listar();
+    }
+    
+    @PostMapping
+    public Rol guardar(@RequestBody Rol rol){
+        return service.guardar(rol);
+    } 
+    
+}
